bea8cba15b180e7db91e73631e7861e49a360795
TanmayDhapodkar <66550499+TanmayDhapodkar@users.noreply.github.com>
2023-06-04 12:05:47 +0530

    Update numeric.py

--- numeric.py
+++ numeric.py
@@ -1,1 +1,54 @@
+#Integer
+num = 3
+print(num)
+
+#Float
+num_2 = 3.14
+print(num_2)
+
+#Type of variable
+print(type(num))
+print((type(num_2)))
+
+#Arithmetic Operators
+print(3+4) #Addition
+print(3-4) #Subtraction
+print(3*4) #Multiplication
+print(3/4) #Division
+print(3//2) #Floor Division - rounds to nearest integer
+print(3**4) #Exponential
+print(3%4) #Modulus - Gets remiander
+
+#Arithermetic Assignment
+num = num +1
+print(num)
+num += 1
+print(num)
+
+#Built in fucntions
+print (abs(-22))
+print(round(3.4))
+print(round(3.142345,3))
+
+#Comparison Operators
+number_1 = 2
+number_2 = 7
+
+print(number_1 == number_2) #Equal To
+print(number_1 != number_2) #Not Equal To
+print(number_2 > number_1) #Greater than
+print(number_2 < number_1) #Less than
+print(number_1 >= number_2) #Greater or equal
+print(number_1 <= number_2) #Less or equal
+
+#Strings concat
+a = '100'
+b = '200'
+print( a + b)
+print(int(a) + int(b))
+
+#Casting of Strings into Integers
+int_a = int(a)
+int_b = int(b)
+print(int_a + int_b)
 
