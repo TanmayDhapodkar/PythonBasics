c3deff4baca07812af2a734b3eb02cd08a66c642
Tanmay Dhapodkar <66550499+TanmayDhapodkar@users.noreply.github.com>
2023-06-07 19:17:10 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -3,12 +3,12 @@
 #Number of days per month, First Value placeholder for indexing purpose
 month_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
-"""Return True for leap years, False for Non Leap years"""
+""" Return True for leap years, False for Non Leap years """
 
 def is_leap(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0 )
 
-""" Return number of days in that month in that year"""
+""" Return number of days in that month in that year """
 
 def days_in_month(year, month):
     if not 1 <= month <= 12:
